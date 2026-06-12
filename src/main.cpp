5a47ef721bdedb3dffafb5181514dc8bd2df2c2b
nguyenthiba021083-source <nguyenthiba021083@gmail.com>
2026-06-12 18:00:42 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,9 +9,16 @@ class $modify(MyMenuLayer, MenuLayer) {
         if (!MenuLayer::init()) return false;
 
         auto winSize = CCDirector::sharedDirector()->getWinSize();
-
-        // Tạo nút bấm kích hoạt Menu AI
+        
         auto sprite = CCSprite::createWithSpriteFrameName("GJ_plusBtn_001.png");
         auto btn = CCMenuItemSpriteExtra::create(
             sprite, this, menu_selector(AIMenu::openMenu)
         );
+
+        auto menu = CCMenu::create(btn, nullptr);
+        menu->setPosition(winSize.width / 2, winSize.height / 2 - 100);
+        this->addChild(menu);
+
+        return true;
+    }
+};
