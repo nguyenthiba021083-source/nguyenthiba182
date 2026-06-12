4607606d6b23af14bd33a250e3d81dda68bebccc
nguyenthiba021083-source <nguyenthiba021083@gmail.com>
2026-06-12 18:18:26 +0700

    Update AIMenu.cpp

--- src/AIMenu.cpp
+++ src/AIMenu.cpp
@@ -19,16 +19,7 @@ bool AIMenu::init() {
     label->setPosition(winSize.width / 2, winSize.height / 2 + 50);
     this->addChild(label);
 
-    // Bật nhận diện phím Back/Escape trên bàn phím
     this->setKeypadEnabled(true);
 
     return true;
-}
-
-// Thêm hàm này để xử lý khi người dùng bấm nút mở Menu
-void AIMenu::openMenu(CCObject* sender) {
-    auto scene = CCScene::create();
-    auto layer = AIMenu::create();
-    scene->addChild(layer);
-    CCDirector::sharedDirector()->pushScene(CCTransitionFade::create(0.5f, scene));
 }
