11def55240fc9f77d375730cd222927d7635fa88
nguyenthiba021083-source <nguyenthiba021083@gmail.com>
2026-06-12 17:46:43 +0700

    Create AISystem.cpp

---
+++ src/AISystem.cpp
@@ -0,0 +1,10 @@
+#include "AISystem.hpp"
+
+using namespace geode::prelude;
+
+void AISystem::generateObjectsFromAI(std::string prompt) {
+    // Thử nghiệm log thông tin ra màn hình console của Geode
+    log::info("AI dang xu ly prompt: {}", prompt);
+    
+    // Code kết nối với API hoặc đọc file JSON AI của bạn sẽ viết ở đây...
+}
