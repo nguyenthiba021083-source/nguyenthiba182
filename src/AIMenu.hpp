d3bbcb4f40620ed8cf4918defb05380523c2b7de
nguyenthiba021083-source <nguyenthiba021083@gmail.com>
2026-06-12 18:14:55 +0700

    Update AIMenu.hpp

--- src/AIMenu.hpp
+++ src/AIMenu.hpp
@@ -3,11 +3,9 @@
 
 using namespace geode::prelude;
 
-// Tạo một lớp layer mới cho Menu AI kế thừa từ CCLayer của Cocos2d
 class AIMenu : public CCLayer {
 protected:
     bool init() override;
 public:
     static AIMenu* create();
-    void openMenu(CCObject* sender); // Hàm để mở menu khi bấm nút
 };
